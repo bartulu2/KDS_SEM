ba97e0646f42c63571da6ab4b8b864c091b1fb0b
Lukáš Bartůněk <bartulu2@fel.cvut.cz>
2022-12-28 17:20:13 +0100

    Added ack, bigger packets

--- rec/UDP_Communication_Framework/UDP_Communication_Framework.cpp
+++ rec/UDP_Communication_Framework/UDP_Communication_Framework.cpp
@@ -10,13 +10,7 @@
 
 #define BUFFERS_LEN 1024
 
-//#define SENDER
 #define RECEIVER
-
-#ifdef SENDER
-#define TARGET_PORT 5555
-#define LOCAL_PORT 8888
-#endif // SENDER
 
 #ifdef RECEIVER
 #define TARGET_PORT 8888
@@ -29,6 +23,15 @@ void InitWinsock()
 	WSADATA wsaData;
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 }
+
+void send_ack(SOCKET socket,char *buffer_tx, sockaddr_in addrDest) {
+	char ack[10];
+	snprintf(ack, 10, "ACK");
+	strncpy(buffer_tx, ack, BUFFERS_LEN);
+	sendto(socket, buffer_tx, strlen(buffer_tx), 0, (sockaddr*)&addrDest, sizeof(addrDest));
+	printf("ACK SEND\n");
+}
+
 
 //**********************************************************************
 int main()
@@ -55,33 +58,22 @@ int main()
 	char buffer_rx[BUFFERS_LEN];
 	char buffer_tx[BUFFERS_LEN];
 
-#ifdef SENDER
-	
 	sockaddr_in addrDest;
 	addrDest.sin_family = AF_INET;
 	addrDest.sin_port = htons(TARGET_PORT);
-	InetPton(AF_INET, _T(TARGET_IP), &addrDest.sin_addr.s_addr);
 
-	
-	strncpy(buffer_tx, "Hello world payload!\n", BUFFERS_LEN); //put some data to buffer
-	printf("Sending packet.\n");
-	sendto(socketS, buffer_tx, strlen(buffer_tx), 0, (sockaddr*)&addrDest, sizeof(addrDest));	
-
-	closesocket(socketS);
-
-#endif // SENDER
-
-#ifdef RECEIVER
 
 	//strncpy(buffer_rx, "No data received.\n", BUFFERS_LEN);
 	memset(buffer_rx, 0x00, sizeof buffer_rx);
 	printf("Waiting for datagram ...\n");
 	char fname[100];
 	char *f_data = NULL;
-	long current_index = 0;
 	FILE* fptr;
 	int index = 0;
 	long data_size = 0;
+	int total_read = 0;
+	int read_amount = 0;
+	bool com_started = false;
 	memset(fname, 0x00, sizeof fname);
 	while (1) {
 		if (recvfrom(socketS, buffer_rx, sizeof(buffer_rx), 0, (sockaddr*)&from, &fromlen) == SOCKET_ERROR) {
@@ -90,9 +82,18 @@ int main()
 			return 1;
 		}
 		else {
-			//printf("Received: %s\n", buffer_rx);
+			send_ack(socketS, buffer_tx, from);
 		}
 		char *pointer = NULL;
+		// Waits for start msg
+		if (strstr(buffer_rx, "ID=3") != NULL) {
+			com_started = true;
+			printf("COM STARTED\n");
+		}
+		if (com_started != true) {
+			printf("Recieved msg without start\n");
+			continue;
+		}
 		//Fetch name of file
 		if (strstr(buffer_rx,"ID=1") != NULL) {
 			pointer = strstr(buffer_rx, "ID=1");
@@ -101,31 +102,27 @@ int main()
 		}
 		//Fetch size of file and allocate memory
 		else if (strstr(buffer_rx, "ID=2")!=NULL){
-			printf("DEBUG:SIZE SAVING\n");
+			//printf("DEBUG:SIZE SAVING\n");
 			pointer = strstr(buffer_rx, "ID=2");
-			char* ptr;
-			char* num_size =(char*) calloc(10, sizeof(char));
-			int i = 0;
 			pointer += 5 * sizeof(char);
-			
 			//printf("ALLOCATED MEMORY, POINTER IS %s\n", pointer);		
 			
-			data_size = strtol(pointer, &ptr, 10);
+			data_size = strtol(pointer, NULL, 10);
 			f_data = (char*)calloc(data_size, sizeof(char));
-			//printf("%d", data_size);
+			printf("DATA_SIZE: %d\n", data_size);
 			memset(f_data, 0x00, data_size * sizeof(char));
 			if (f_data == NULL) {
 				printf("ERROR: cannot allocate memory.\n");
 				exit(1);
 			}
-			free(num_size);
 		}
 		//Fetch data and save it to file_buffer
 		else if (strstr(buffer_rx, "ID=4")) {
+			
 			//printf("DEBUG DATA\n");
 			pointer = strstr(buffer_rx, "ID=4");
 			char* num_bytes = (char*)calloc(10, sizeof(char));
-			pointer = pointer + 5 * sizeof(char);
+			pointer += 5 * sizeof(char);
 			int i = 0;
 			while (pointer[0] != '|') {
 				num_bytes[i] = pointer[0];
@@ -133,37 +130,41 @@ int main()
 				pointer += sizeof(char);
 			}
 			
-			int read_amount = strtol(num_bytes, NULL, 10);
-			char* data_ptr = (char*)calloc(read_amount, sizeof(char));
-			//printf("READ AMOUNT OF DATA %d\n",read_amount);
+			read_amount = strtol(num_bytes, NULL, 10);
 			pointer += sizeof(char);
-			//printf("Received %d amount, index at %d\n",read_amount,index);
-			for (int i = 0;i < read_amount;i=i+1) {
-				data_ptr[i] = pointer[i];
+			for (int i = 0;i < read_amount;i++) {
+				f_data[i+total_read] = pointer[i];
 			}
-			//printf("Received %s data of size %dB\n", data_ptr,read_amount);
-			for (int i = 0;i < read_amount;i+=1) {
-				//printf("saving %s to index %d\n", &data_ptr[i], index + i);
-				f_data[index+i] = data_ptr[i];
+			total_read += read_amount;
+			index++;
+
+			// PRINT KONTROLA
+			if (read_amount == 0) {
+				com_started = false;
 			}
-			
-			index += 1;
-			
-			free(num_bytes);
-			free(data_ptr);
+			else {
+				printf("READ AMOUNT: %d, INDEX: %d\n", read_amount, index);
+				//printf("DATA:\n");
+				//for (i = 0; i < read_amount; i++) {
+				//	printf("%c", pointer[i]);
+				//}
+				//printf("\n");
+			}
 		}
-		if (index * sizeof(char) == data_size && data_size!=0 ) {
-			//printf("KONEC, index je %d, velikost souboru je %d\n",index,index*sizeof(char));
-			fptr = fopen(fname, "wb+");
-			//printf("last char is %s", &f_data[index - 2]);
-			int ret = fwrite(f_data, sizeof(char),index*sizeof(char), fptr);
+		if (total_read == data_size && data_size!=0 && read_amount ==0) {
+			printf("KONEC, index je %d, velikost souboru je %d\n",index,data_size);
+			fptr = fopen(fname, "wb");
+			int ret = fwrite(f_data, sizeof(char), total_read, fptr);
+			if (ret == NULL) {
+				printf("You fucked up");
+			}
 			fclose(fptr);
+			printf("Finished writting in file");
 		}
 		
 		memset(buffer_rx, 0, sizeof buffer_rx);
 	}
 	closesocket(socketS);
-#endif
 	//**********************************************************************
 
 	getchar(); //wait for press Enter
--- send/UDP_Communication_Framework/UDP_Communication_Framework.cpp
+++ send/UDP_Communication_Framework/UDP_Communication_Framework.cpp
@@ -5,23 +5,16 @@
 #include "stdafx.h"
 #include <winsock2.h>
 #include "ws2tcpip.h"
+#include <iostream>
 
 #define TARGET_IP	"127.0.0.1"
 
 #define BUFFERS_LEN 1024
 
 #define SENDER
-//#define RECEIVER
 
-#ifdef SENDER
 #define TARGET_PORT 5555
 #define LOCAL_PORT 8888
-#endif // SENDER
-
-#ifdef RECEIVER
-#define TARGET_PORT 8888
-#define LOCAL_PORT 5555
-#endif // RECEIVER
 
 
 
@@ -31,33 +24,46 @@ void InitWinsock()
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 }
 
+bool recieve_ack(SOCKET socket, char *buffer_rx, sockaddr_in dest ) {
+	int destlen = sizeof(dest);
+	//printf("WAITING FOR ACK\n");
+	if (recvfrom(socket, buffer_rx, sizeof(buffer_rx), 0, (sockaddr*)&dest, &destlen) == SOCKET_ERROR) {
+		printf("Socket error\n");
+		getchar();
+		return 1;
+	}
+	else {
+		printf("Received ACK\n");
+	}
+}
+
+
 //**********************************************************************
 int main()
 {
 	SOCKET socketS;
-	
+
 	InitWinsock();
 
 	struct sockaddr_in local;
 	struct sockaddr_in from;
 
-	int fromlen = sizeof(from);
+	
 	local.sin_family = AF_INET;
 	local.sin_port = htons(LOCAL_PORT);
 	local.sin_addr.s_addr = INADDR_ANY;
 
 
 	socketS = socket(AF_INET, SOCK_DGRAM, 0);
-	if (bind(socketS, (sockaddr*)&local, sizeof(local)) != 0){
+	if (bind(socketS, (sockaddr*)&local, sizeof(local)) != 0) {
 		printf("Binding error!\n");
-	    getchar(); //wait for press Enter
+		getchar(); //wait for press Enter
 		return 1;
 	}
 	//**********************************************************************
 	char buffer_rx[BUFFERS_LEN];
 	char buffer_tx[BUFFERS_LEN];
 
-#ifdef SENDER
 	// ID=1 NAME
 	// ID=2 SIZE
 	// ID=3 START
@@ -71,54 +77,73 @@ int main()
 	addrDest.sin_family = AF_INET;
 	addrDest.sin_port = htons(TARGET_PORT);
 	InetPton(AF_INET, _T(TARGET_IP), &addrDest.sin_addr.s_addr);
-	char name[] = "..\\headlol.png";
+	char name[] = "..\\jpg.jpg";
 	printf("%s\n", name);
-	if ((fptr = fopen(name, "rb+")) == NULL) {
+	if ((fptr = fopen(name, "rb")) == NULL) {
 		printf("Error! opening file");
 
 		// Program exits if the file pointer returns NULL.
 		exit(1);
 	}
 	printf("Opened file succesfully\n");
-	char src[50];
-	char fur[50];
-	snprintf(src,50,"ID=%d|%s",1,"head.png");
-	//SEND NAME=filename + type
-	strncpy(buffer_tx, src, BUFFERS_LEN);
-	printf("Sending file name %s\n",src);
-	//printf("Buffer is %s\n", buffer_tx);
-	sendto(socketS, buffer_tx, strlen(buffer_tx), 0, (sockaddr*)&addrDest, sizeof(addrDest));
-	memset(buffer_tx, 0, sizeof buffer_tx);
-	
-	//SEND SIZE OF FILE
-	
-	fseek(fptr, 0L, SEEK_END);
-	char buffer[100];
-	long int res = ftell(fptr)/sizeof(char);
-	fseek(fptr, 0L, SEEK_SET);
-	int f_size = snprintf(buffer, 100, "ID=%d|%d", 2,res);
-
-	strncpy(buffer_tx, buffer , BUFFERS_LEN);
-	printf("Sending file size %sB\n",buffer);
-	//printf("Buffer is %s\n", buffer_tx);
-	sendto(socketS, buffer_tx, strlen(buffer_tx), 0, (sockaddr*)&addrDest, sizeof(addrDest));
-	memset(buffer_tx, 0, sizeof buffer_tx);
-	
 	//SEND START
-	
+
 	char start[10];
 	snprintf(start, 10, "ID=%d", 3);
-	strncpy(buffer_tx,start, BUFFERS_LEN);
+	strncpy(buffer_tx, start, BUFFERS_LEN);
 	//printf("Buffer is %s\n", buffer_tx);
 	sendto(socketS, buffer_tx, strlen(buffer_tx), 0, (sockaddr*)&addrDest, sizeof(addrDest));
 	printf("Sending START \n");
 	memset(buffer_tx, 0, sizeof buffer_tx);
-	
+
+	if (!recieve_ack(socketS, buffer_rx, addrDest)) {
+		sendto(socketS, buffer_tx, strlen(buffer_tx), 0, (sockaddr*)&addrDest, sizeof(addrDest));
+		printf("Resending START \n");
+		memset(buffer_tx, 0, sizeof buffer_tx);
+	}
+
+	char src[50];
+	char fur[50];
+	snprintf(src, 50, "ID=%d|%s", 1, "jpg.jpg");
+	//SEND NAME=filename + type
+	strncpy(buffer_tx, src, BUFFERS_LEN);
+	printf("Sending file name %s\n", src);
+	//printf("Buffer is %s\n", buffer_tx);
+	sendto(socketS, buffer_tx, strlen(buffer_tx), 0, (sockaddr*)&addrDest, sizeof(addrDest));
+	memset(buffer_tx, 0, sizeof buffer_tx);
+
+	if (!recieve_ack(socketS, buffer_rx, addrDest)) {
+		sendto(socketS, buffer_tx, strlen(buffer_tx), 0, (sockaddr*)&addrDest, sizeof(addrDest));
+		printf("Resending file name %s\n",src);
+		memset(buffer_tx, 0, sizeof buffer_tx);
+	}
+
+	//SEND SIZE OF FILE
+
+	fseek(fptr, 0L, SEEK_END);
+	char buffer[100];
+	long int res = ftell(fptr) / sizeof(char);
+	fseek(fptr, 0L, SEEK_SET);
+	int f_size = snprintf(buffer, 100, "ID=%d|%d", 2, res);
+
+	strncpy(buffer_tx, buffer, BUFFERS_LEN);
+	printf("Sending file size %sB\n", buffer);
+	//printf("Buffer is %s\n", buffer_tx);
+	sendto(socketS, buffer_tx, strlen(buffer_tx), 0, (sockaddr*)&addrDest, sizeof(addrDest));
+	memset(buffer_tx, 0, sizeof buffer_tx);
+
+	if (!recieve_ack(socketS, buffer_rx, addrDest)) {
+		sendto(socketS, buffer_tx, strlen(buffer_tx), 0, (sockaddr*)&addrDest, sizeof(addrDest));
+		printf("Resending file size %sB\n", buffer);
+		memset(buffer_tx, 0, sizeof buffer_tx);
+	}
+
+
 	//START SENDING FILE DATA
 
-	char data_buffer[1000*sizeof(char)];
+	char data_buffer[1000 * sizeof(char)];
 	char packet_buffer[1024];
-	char numbits[8];
+	char numbits[25];
 	char header[25];
 	long data_pointer = 0;
 	long bytes_sent = 0;
@@ -127,53 +152,40 @@ int main()
 	memset(packet_buffer, 0x00, sizeof packet_buffer);
 	memset(data_buffer, 0x00, sizeof data_buffer);
 	while (cont) {
-		int bytes_read = fread(data_buffer,sizeof(char), 1*sizeof(char), fptr);
+		int bytes_read = fread(data_buffer, sizeof(char), 900 * sizeof(char), fptr);
 		snprintf(header, 23, "ID=%d|", 4);
 
 		snprintf(numbits, 20, "%d", bytes_read);
-		
+
 		strcat(header, numbits);
 		strcat(header, "|");
 		strcat(packet_buffer, header);
 		strcat(packet_buffer, data_buffer);
-		
-		
+
+
 		sendto(socketS, packet_buffer, strlen(packet_buffer), 0, (sockaddr*)&addrDest, sizeof(addrDest));
-		//printf("Sending DATA %s, read %d bytes\n",data_buffer,bytes_read);
+		printf("Sending DATA, SIZE: %d bytes\n", bytes_read);
+
+		if (!recieve_ack(socketS, buffer_rx, addrDest)) {
+			sendto(socketS, buffer_tx, strlen(buffer_tx), 0, (sockaddr*)&addrDest, sizeof(addrDest));
+			printf("Resending DATA, SIZE %d bytes\n", bytes_read);
+			memset(buffer_tx, 0, sizeof buffer_tx);
+		}
+
 		bytes_sent += bytes_read;
 		memset(data_buffer, 0x00, sizeof data_buffer);
 		memset(packet_buffer, 0x00, sizeof packet_buffer);
 		memset(header, 0x00, sizeof header);
 		memset(numbits, 0x00, sizeof numbits);
-		if (bytes_read == 0){
+		if (bytes_read == 0) {
 			cont = false;
 		}
 		epoch -= 1;
 	}
 	//SEND STOP TO FINISH
 	printf("Sent total of %ldB", bytes_sent);
-		
+
 
 	closesocket(socketS);
-
-#endif // SENDER
-
-#ifdef RECEIVER
-
-	strncpy(buffer_rx, "No data received.\n", BUFFERS_LEN);
-	printf("Waiting for datagram ...\n");
-	if(recvfrom(socketS, buffer_rx, sizeof(buffer_rx), 0, (sockaddr*)&from, &fromlen) == SOCKET_ERROR){
-		printf("Socket error!\n");
-		getchar();
-		return 1;
-	}
-	else
-		printf("Datagram: %s", buffer_rx);
-
-	closesocket(socketS);
-#endif
-	//**********************************************************************
-
-	getchar(); //wait for press Enter
-	return 0;
 }
+
